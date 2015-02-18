60ccd33041e1ae5a1e669a74d08dd7c63f531b57
Robert Davis <best.theory84@gmail.com>
2015-02-18 12:58:56 -0600

    Update validation.js

--- validation.js
+++ validation.js
@@ -1,36 +1,31 @@
-function validateMe() {
-    // These are regex to check for valid email and phone number
-    var emailVal = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
-    var phoneVal = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
-    if($(this).hasClass('email')) {
-        if (!(emailVal.test($(this).val()))) {
-            $(this).removeClass('correct').addClass('error');
-        } else {
-            $(this).removeClass('error').addClass('correct');	
-        }					
-    }
-    else if($(this).hasClass('phone')) {
-        if (!(phoneVal.test($(this).val()))) {
-            $(this).removeClass('correct').addClass('error');
-        } else {
-            $(this).removeClass('error').addClass('correct');	
-        }		
-    }
-	else if($(this).val() == "") {
-		$(this).removeClass('correct').addClass('error')
-	}
-	else { 
-		$(this).removeClass('error').addClass('correct')
-	}	
-}	
-
-
 $(document).ready(function() {
-    // This is for on submit
+    	// This is for on submit
 	$("#form").submit(function() {
-        $('input[type=text], input[type=tel], input[type=email]').each(function() {
-		  validateMe();
-        }
+        	$('.required').each(function() {
+		// These are regex to check for valid email and phone number
+    		var emailVal = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    		var phoneVal = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+		    if($(this).hasClass('email')) {
+		        if (!(emailVal.test($(this).val()))) {
+		            $(this).removeClass('correct').addClass('error');
+		        } else {
+		            $(this).removeClass('error').addClass('correct');	
+		        }					
+		    }
+		    else if($(this).hasClass('phone')) {
+		        if (!(phoneVal.test($(this).val()))) {
+		            $(this).removeClass('correct').addClass('error');
+		        } else {
+		            $(this).removeClass('error').addClass('correct');	
+		        }		
+		    }
+			else if($(this).val() == "") {
+				$(this).removeClass('correct').addClass('error')
+			}
+			else { 
+				$(this).removeClass('error').addClass('correct')
+			}
+        	}
 		var errors = $('input.error').length;
 		if(errors !== 0) {
 			alert('Please fill out all required fields');
@@ -38,8 +33,29 @@ $(document).ready(function() {
 		}
 	});
 	// This is for on blur
-    $('input[type=text], input[type=tel], input[type=email]').blur(function () {
-		validateMe();
-    });
-    
+    	$('.required').blur(function () {
+		// These are regex to check for valid email and phone number
+	    	var emailVal = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+	    	var phoneVal = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+	    	if($(this).hasClass('email')) {
+	        	if (!(emailVal.test($(this).val()))) {
+	            		$(this).removeClass('correct').addClass('error');
+	        	} else {
+	            		$(this).removeClass('error').addClass('correct');	
+	        	}					
+	    	}
+	    	else if($(this).hasClass('phone')) {
+	        	if (!(phoneVal.test($(this).val()))) {
+	            		$(this).removeClass('correct').addClass('error');
+	        	} else {
+	            		$(this).removeClass('error').addClass('correct');	
+	        	}		
+	    	}
+		else if($(this).val() == "") {
+			$(this).removeClass('correct').addClass('error')
+		}
+		else { 
+			$(this).removeClass('error').addClass('correct')
+		}
+    	});
 });
